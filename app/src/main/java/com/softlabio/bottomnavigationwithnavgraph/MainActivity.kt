9167db7065c839669870316b4489b8dcacdede20
ASA-Lab <subarata.talukder@asabd.org>
2021-09-19 20:02:11 +0600

    Project initiated

---
+++ app/src/main/java/com/softlabio/bottomnavigationwithnavgraph/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.softlabio.bottomnavigationwithnavgraph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
