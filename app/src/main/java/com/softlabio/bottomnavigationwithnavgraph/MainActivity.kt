11f7ab082b75bf499a7036cfc58b36b89999c59b
ASA-Lab <subarata.talukder@asabd.org>
2021-09-19 20:47:29 +0600

    bottom nav structure done

--- app/src/main/java/com/softlabio/bottomnavigationwithnavgraph/MainActivity.kt
+++ app/src/main/java/com/softlabio/bottomnavigationwithnavgraph/MainActivity.kt
@@ -2,10 +2,17 @@ package com.softlabio.bottomnavigationwithnavgraph
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        bottomNavigationView.setupWithNavController(navHostFragment.navController)
     }
 }
